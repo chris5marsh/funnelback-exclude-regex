dd3992750316ce534dfd51497cab2a67a3f021c5
Chris Marsh <chris.marsh@york.ac.uk>
2019-05-02 13:53:37 +0100

    Move to Connect server and simple front-end

---
+++ convert.js
@@ -0,0 +1,82 @@
+/*eslint node: true */
+"use strict";
+
+const fs = require("fs");
+//const path = require("path");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+const console = require("better-console");
+
+module.exports = (data, onComplete) => {
+
+  const datestring = Date.now();
+  const inputFile = "input.txt";
+  const outputFile = "output-"+datestring+".txt"
+
+  // write data to input file, ready for next time
+  writeFile(inputFile, data, "utf-8").then((err) => {
+
+    if (err) return onComplete(false);
+
+    console.info("Successfully wrote original data to "+inputFile);
+
+    // Ignore all sub matches
+    // Find new RegExp("...") and save them
+    const re = /new RegExp\("(.*)"\)/g;
+    let reArray;
+    const results = [];
+    let i = 0;
+
+    while ((reArray = re.exec(data)) !== null) {
+       results[i] = reArray[1];
+       data = data.replace(reArray[0], "==="+i+"===");
+       i++;
+    }
+
+    // Replace line breaks with "|"
+    data = data.replace(/\n|\r/g, "|");
+    // Replace "?" with "\?"
+    data = data.replace(/\?/g, "\\?");
+    // Replace "/" with "\/"
+    data = data.replace(/\//g, "\\/");
+    // Replace "." with "\."
+    data = data.replace(/\./g, "\\.");
+    // Replace "*" with "\*"
+    data = data.replace(/\*/g, "\\*");
+    // Replace "$" with "\$"
+    data = data.replace(/\$/g, "\\$");
+    // Add "regexp:" to front
+    data = "regexp:"+data;
+
+    // Remove final "|" if present
+    if (data.substr(-1) === "|") {
+      data = data.substring(0, data.length-1);
+    }
+
+    // Replace regexes
+    //var reg;
+    let j = 0;
+    for (;j < results.length; j++) {
+      var find = "==="+j+"===";
+      data = data.replace(find, results[j]);
+    }
+
+    writeFile(outputFile, data, "utf-8").then((err) => {
+
+      if (err) return onComplete(false);
+
+      console.info("Successfully wrote updated data to "+outputFile);
+
+      return onComplete(outputFile);
+
+    }).catch((err) => {
+
+      console.error("There was an error: "+err);
+
+      return onComplete(false);
+
+    });
+
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,44 @@
+/*eslint node: true */
+"use strict";
+
+var connect = require('connect');
+var http = require('http');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+var convert = require('./convert');
+var console = require('better-console');
+
+var app = connect();
+
+// gzip/deflate outgoing responses
+app.use(compression());
+
+// parse urlencoded request bodies into req.body
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Deal with calls to /api
+app.use('/api', function api(req, res, next) {
+  console.log("API called");
+  if (req.body.value) {
+    convert(req.body.value, (r) => {
+      //console.log("r is "+r);
+      if (r !== false) {
+        // send content back as text file
+        res.end('{ "success" : "'+r+'" }');
+        next();
+      } else {
+        res.end('{ "error" : "The form was not filled in properly" }');
+        next();
+      }
+    })
+  } else {
+    res.end('{ "error" : "The form was not filled in properly" }');
+    next();
+  }
+});
+
+// Deal with all other static files
+app.use(serveStatic(__dirname)).listen(8080, function(){
+  console.info('Server running on 8080...');
+});
